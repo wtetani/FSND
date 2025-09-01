8c423d4efa973c43486e4de7699029c403a6eb5e
Witness Tetani <witness.tetani@gmail.com>
2025-09-01 20:25:40 +0200

    Changes to show and venue models

--- projects/01_fyyur/app/models/show.py
+++ projects/01_fyyur/app/models/show.py
@@ -1,15 +1,15 @@
 from ..extensions import db
+from sqlalchemy.schema import ForeignKey
 from sqlalchemy.orm import relationship
 
-
 class Show(db.Model):
-  __tablename__ = 'Musical Show'  
-  venue_id = db.Column(db.Integer, db.ForeignKey('venue_id'), nullable=False)
-  artist_id = db.Column(db.Integer, db.ForeignKey('artist_id'), nullable=False)
+  __tablename__ = 'show'  
+  venue_id = db.Column(ForeignKey('venue.venue_id'), primary_key=True)
+  artist_id = db.Column(ForeignKey('artist.artist_id'), primary_key=True)
   start_time = db.Column(db.DateTime(timezone=True), nullable=False)
   end_time = db.Column(db.DateTime(timezone=True), nullable=False)
-  Artist = relationship('Artist', back_populates='Venue')
-  Venue = relationship('Venue', back_populates='Artist')
+  artists = relationship('Artist', back_populates='venues')
+  venues = relationship('Venue', back_populates='artists')
 
   def __repr__(self):
     return f'<Show Venue: {self.Venue}, Artist: {self.Artist}, Start-time: {self.start_time}, End-time:{self.end_time}>'
--- projects/01_fyyur/app/models/venue.py
+++ projects/01_fyyur/app/models/venue.py
@@ -1,9 +1,8 @@
 from ..extensions import db
 from sqlalchemy.orm import relationship
 
-
 class Venue(db.Model):
-  __tablename__ = 'Venue'
+  __tablename__ = 'venue'
   venue_id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String, nullable=False)
   city = db.Column(db.String(120), nullable=False)
@@ -17,7 +16,7 @@ class Venue(db.Model):
   venue_website = db.Column(db.String(120))
   seeking_talent = db.Column(db.Boolean, default=False)
   talent_description = db.Column(db.String(120))
-  Artist = relationship('Show', back_populates='Venue')
+  artists = relationship('Show', back_populates='venues')
 
   def __repr__(self):
     return f'<Venue: {self.name}, Phone: {self.phone}, City: {self.city}, State: {self.state}>'
